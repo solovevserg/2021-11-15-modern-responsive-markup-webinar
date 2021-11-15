facb0dfb64816d86554aa5af92719ea5b3c6442b
Sergei Solovev <sergey.soloviev@inbox.ru>
2021-11-15 22:04:44 +0300

    code written before webinar

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const MOBILE_TO_DESKTOP = 850;
+const SCROLL_DELAY = 80;
+
+const body = document.body
+function resize() {
+    const width = window.innerWidth;
+    const isMobile = width < MOBILE_TO_DESKTOP;
+    body.classList.toggle('mobile', isMobile);
+    body.classList.toggle('desktop', !isMobile);
+}
+
+const nav = document.querySelector('nav');
+function updateNav() {
+    const scrollY = window.scrollY;
+    nav.classList.toggle('nav--scrolled', scrollY > SCROLL_DELAY)
+}
+
+async function main() {
+    window.addEventListener('resize', resize);
+    resize();
+    window.addEventListener('scroll', updateNav)
+    updateNav()
+}
+
+main();
