176e876dc2d26942bfd94121a6ca4a70ea6964a1
Artem <s18b3_vlasenko@179.ru>
2021-11-21 15:01:13 +0300

    set idents equal 2 spaces

--- src/js/main.js
+++ src/js/main.js
@@ -7,45 +7,45 @@ const nav = document.querySelector("header");
 let scrollTimeout = null;
 
 function resize() {
-    const width = window.innerWidth;
-    body.classList.toggle("mobile", width < MOBILE_TO_TABLET);
-    body.classList.toggle(
-        "tablet",
-        (width >= MOBILE_TO_TABLET) & (width < TABLET_TO_DESKTOP)
-    );
-    body.classList.toggle("desktop", width >= TABLET_TO_DESKTOP);
+  const width = window.innerWidth;
+  body.classList.toggle("mobile", width < MOBILE_TO_TABLET);
+  body.classList.toggle(
+    "tablet",
+    (width >= MOBILE_TO_TABLET) & (width < TABLET_TO_DESKTOP)
+  );
+  body.classList.toggle("desktop", width >= TABLET_TO_DESKTOP);
 }
 
 function updateNav() {
-    const scrollY = window.scrollY;
-    nav.classList.toggle("nav--scrolled", scrollY > SCROLL_DELAY);
-    if (scrollTimeout) {
-        clearTimeout(scrollTimeout);
-        scrollTimeout = null;
-    }
-    scrollTimeout = setTimeout(function () {
-        body.querySelectorAll("section").forEach((elem) => {
-            if (
-                elem.offsetTop < scrollY + 200 && // Выше секции
-                elem.offsetTop + elem.scrollHeight > scrollY + 10 // Но она умещается целиком
-            ) {
-                const sectionName = elem.getAttribute("id");
-                location.hash = "#" + sectionName;
-                if (!body.classList.contains("desktop")) {
-                    // Обновляем секцию в select (если не desktop)
-                    const selectSection = body.querySelector("#select-section");
-                    selectSection.value = sectionName;
-                }
-            }
-        });
-    }, 100);
+  const scrollY = window.scrollY;
+  nav.classList.toggle("nav--scrolled", scrollY > SCROLL_DELAY);
+  if (scrollTimeout) {
+    clearTimeout(scrollTimeout);
+    scrollTimeout = null;
+  }
+  scrollTimeout = setTimeout(function () {
+    body.querySelectorAll("section").forEach((elem) => {
+      if (
+        elem.offsetTop < scrollY + 200 && // Выше секции
+        elem.offsetTop + elem.scrollHeight > scrollY + 10 // Но она умещается целиком
+      ) {
+        const sectionName = elem.getAttribute("id");
+        location.hash = "#" + sectionName;
+        if (!body.classList.contains("desktop")) {
+          // Обновляем секцию в select (если не desktop)
+          const selectSection = body.querySelector("#select-section");
+          selectSection.value = sectionName;
+        }
+      }
+    });
+  }, 100);
 }
 
 async function main() {
-    window.addEventListener("resize", resize);
-    resize();
-    window.addEventListener("scroll", updateNav);
-    updateNav();
+  window.addEventListener("resize", resize);
+  resize();
+  window.addEventListener("scroll", updateNav);
+  updateNav();
 }
 
 main();
